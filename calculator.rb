4ffac41318acedc1145566233ff8a8b81ce7b1c0
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-21 04:36:36 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,7 @@
 # Add your variables here
 first_number = 5
 second_number = 2
-def calculator(first_number, second_number)
-  sum = first_number + second_number
-end
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
